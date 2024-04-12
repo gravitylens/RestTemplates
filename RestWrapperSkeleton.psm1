2d6929ef4ffb9d479b320b7301d91405d0f41dab
gravitylens <31743459+gravitylens@users.noreply.github.com>
2024-04-12 08:06:33 -0500

    Simple and Complex examples

--- RestWrapperSkeleton.psm1
+++ RestWrapperSkeleton.psm1
@@ -3,7 +3,53 @@ $global:headers = New-Object "System.Collections.Generic.Dictionary[[String],[St
 $global:headers.Add("Content-Type", "application/json")
 $global:url = "https://pvwa.acme.corp"
 
-function FunctionName {
+function SimpleFunctionName {
+    param (
+        #Collect first parameter from command line or pipeline
+        [Parameter(
+            ValueFromPipeline=$true,
+            ValueFromPipelineByPropertyName=$true,
+            Mandatory=$true
+        )]
+        [string]$Param1,
+
+        #Collect second parameter from command line or pipeline
+        [Parameter(
+            ValueFromPipeline=$true,
+            ValueFromPipelineByPropertyName=$true,
+            Mandatory=$true
+        )]
+        [string]$Param2
+    )
+
+    $uri="$url/API/method"      #Append URI to the URL to identify the specific Rest Method
+    $method="POST"              #HTTP Verb for the Rest Method
+
+    $body=@{                    #Assemble body from parameters
+        "Attribute1" = $Param1
+        "Attribute2" = $Param2
+    } | ConvertTo-Json          #Convert to JSON for Rest
+
+    TRY{
+        #Invoke the Rest Method and assign the response to $response
+        $response=Invoke-RestMethod -Uri $uri -Method $method -Headers $headers -Body $body
+    }
+    CATCH{
+        #If the Rest Method fails, assign an appropriate error message to $response
+        $response = "ErrorCode: " + $_.Exception.Response.StatusCode.value__ + "`n"
+        $response += "Uri: " + $uri + "`n"
+        $response += "Method: " + $method + "`n"
+        $response += "Body: " + $body + "`n"
+        $response += "Headers: `n"# + $headers.GetEnumerator() | ForEach-Object { $_.Value }
+        foreach ($key in $headers.Keys) { 
+            $response += "$key -> $($headers[$key])`n" 
+        } 
+    }
+    #Return Rest response or error message as necessary.
+    return $response;    
+}
+
+function PipelineFunctionName {
     param (
         #Collect first parameter from command line or pipeline
         [Parameter(
